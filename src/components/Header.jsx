b9eaa65254c52cb44f88300dc131148bfb0d0e7b
hoi-ee04 <chaehl1004@gmail.com>
2024-10-02 16:58:57 +0900

    init: 헤더 제작

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { ReactComponent as Logo } from "../images/airplaneLogo.svg";
+import { ReactComponent as Logout } from "../images/logoutButton.svg";
+
+const Header = () => {
+  return (
+    <HeaderContent>
+      <WholeContainer>
+        <LogoComponent>
+          <LogoText>비행기레터</LogoText>
+          <Logo />
+        </LogoComponent>
+        <Logout />
+      </WholeContainer>
+    </HeaderContent>
+  );
+};
+
+const HeaderContent = styled.div`
+  display: flex;
+  position: fixed;
+  z-index: 1100;
+  width: 100vw;
+  height: 110px;
+  align-items: center;
+  background-color: white;
+  border-bottom: 1px solid #a9a9a9;
+`;
+
+const WholeContainer = styled.div`
+  display: flex;
+  padding: 0 18.75vw;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoComponent = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 170px;
+`;
+
+const LogoText = styled.p`
+  color: ${({ theme }) => theme.colors.blue};
+  ${({ theme }) => theme.fonts.heading};
+`;
+
+export default Header;

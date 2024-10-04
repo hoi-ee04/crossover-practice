3f08c17e3e74b0f3247a57d922fd52fee956d360
hoi-ee04 <chaehl1004@gmail.com>
2024-10-05 01:31:35 +0900

    init: 로그인 페이지 유효성 검사, 아이콘 작동 반영

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,24 +1,49 @@
-import { useState } from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import { ReactComponent as CancelIcon } from "../images/cancelButton.svg";
+import { ReactComponent as ErrorIcon } from "../images/errorCircle.svg";
+import { ReactComponent as SuccessIcon } from "../images/SuccessCircle.svg";
 
-const InputBox = ({ inputText, explainText }) => {
+const InputBox = ({ inputText, explainText, validate, isPassword }) => {
   const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
   const handleChange = (e) => {
-    setValue(e.target.value);
+    const inputValue = e.target.value;
+    setValue(inputValue);
+
+    if (inputValue) {
+      setIsValid(validate(inputValue));
+    } else {
+      setIsValid(false);
+    }
+  };
+
+  const handleCancel = () => {
+    setValue("");
+    setIsValid(false);
+  };
+
+  const getBorderColor = () => {
+    if (!value) return "gray";
+    return isValid ? "green" : "red";
   };
 
   return (
     <InputBoxContainer>
-      <InnerContainer>
+      <InnerContainer borderColor={getBorderColor()}>
         <InputText
-          type="text"
+          type={isPassword ? "password" : "text"}
           value={value}
           placeholder={value ? "" : inputText}
           onChange={handleChange}
         />
+        <div>
+          {value && (isValid ? <Success /> : <Error />)}
+          {value && <Cancel onClick={handleCancel} />}
+        </div>
       </InnerContainer>
-      <ExplainText>{explainText}</ExplainText>
+      <ExplainText textColor={getBorderColor()}>{explainText}</ExplainText>
     </InputBoxContainer>
   );
 };
@@ -36,8 +61,9 @@ const InnerContainer = styled.div`
   min-width: 100px;
   height: 90px;
   border-radius: 1.08vw;
-  border: 2px solid ${({ theme }) => theme.colors.gray};
+  border: 2px solid ${({ theme, borderColor }) => theme.colors[borderColor]};
   padding: 5.18%;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -48,11 +74,24 @@ const InputText = styled.input`
   opacity: 45%;
 `;
 
+const Error = styled(ErrorIcon)`
+  margin: 5px;
+`;
+
+const Success = styled(SuccessIcon)`
+  margin: 5px;
+`;
+
+const Cancel = styled(CancelIcon)`
+  margin: 5px;
+`;
+
 const ExplainText = styled.p`
   ${({ theme }) => theme.fonts.helperText};
   margin: 10px 0;
   margin-left: 5px;
   opacity: 45%;
+  color: ${({ theme, textColor }) => theme.colors[textColor]};
 `;
 
 export default InputBox;

fd3220554d5e2a10eb5a87651a7301b30488fb8d
hoi-ee04 <chaehl1004@gmail.com>
2024-10-05 00:10:20 +0900

    임시: path="/" 로그인 페이지로 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <BrowserRouter>
       <Layout />
       <Routes>
-        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
